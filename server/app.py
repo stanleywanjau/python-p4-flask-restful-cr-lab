7d2fcccedb0bc29e562653aca0152c776079d646
stanleywanjau <stanleywanjau371@gmail.com>
2024-01-26 09:46:17 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -17,10 +17,37 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = [ p.to_dict() for p in Plant.query.all()]
+        
+        response=make_response(plants,200)
+        
+        return response
+    
+    def post(self):
+        new_plant=Plant(
+            name=request.form['name'],
+            image=request.form['image'],
+            price=request.form['price'],
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        response_dict = new_plant.to_dict()
+        response=make_response(response_dict,201)
+        return response
+        
+api.add_resource(Plants,'/plants')
+        
 
 class PlantByID(Resource):
-    pass
+    def get(self,id):
+        plant= Plant.query.filter_by(id=id).first().to_dict()
+    
+        response=make_response(plant,200)
+    
+        return response
+    
+api.add_resource(PlantByID,'/plants/<int:id>')
         
 
 if __name__ == '__main__':
